8634b46769819c16ad1349d521015ac80c00ae1d
THE TOP BOY <160314952+THE-TOP-BOY@users.noreply.github.com>
2024-03-03 16:22:45 +0530

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/itz_lucky_raja"
             ),
         ],
         [
@@ -29,7 +29,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/itz_lucky_Raja",
             )
         ],
         [
