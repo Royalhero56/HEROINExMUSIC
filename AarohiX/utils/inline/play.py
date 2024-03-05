2a48408221ef9c1aea0858c77ba8eabf149263c7
THE TOP BOY <160314952+THE-TOP-BOY@users.noreply.github.com>
2024-03-05 23:21:09 +0530

    Update play.py

--- AarohiX/utils/inline/play.py
+++ AarohiX/utils/inline/play.py
@@ -40,8 +40,8 @@ def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-top = math.floor(percentage)
-    if 0 < top <= 10:
+    top = math.floor(percentage)
+      if 0 < top <= 10:
         bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ❤️"
     elif 10 < top < 20:
         bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ❤️٨ـ"
