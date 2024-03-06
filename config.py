e6ffccb21ebb191f7cd7467bf7bc83e2353473c1
THE TOP BOY <160314952+THE-TOP-BOY@users.noreply.github.com>
2024-03-06 13:53:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-TOP-BOY/LuckyXMusic",
+    "https://github.com/Royalhero56/HEROINExMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
